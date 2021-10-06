c75ba773015495130aad4dac91fd91f8e786fa54
Abhinav <abhinavarya005522@gmail.com>
2021-10-06 14:52:55 +0530

    Initial commit

---
+++ connectdb.php
@@ -0,0 +1,13 @@
+
+    <div class="nav">
+        <h1>   Registration form     </h1>
+    </div>
+    <div>
+        <form action="#" method="POST">
+            name : <input type="text" id="name">
+            email : <input type="text" id="email">
+            phone : <input type="number" id="phone">
+            <input type="password" id="password">
+            <input type="button" value="Submit">
+        </form>
+    </div>
